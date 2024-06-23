16a54e26816d577fb4442b1b8e75fdaa2a1122a6
Katrin <kateyka1410@gmail.com>
2024-06-23 21:36:39 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,36 +1,35 @@
-
-const refs = {
-imageForm: document.querySelector('.form'),
-imageInput: document.querySelector('.input'),
-submitButton: document.querySelector('.btn'),
-imageList: document.querySelector('.images-list'),
+export function photoTemplate(image) {
+    return `<li class="gallery-item">
+      <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" class="gallery-item-image"></a>
+        <ul class="image-info-list">
+          <li class="list-item">
+            <h3>Likes</h3>
+            <p class="likes-js">${image.likes}</p>
+          </li>
+          <li class="list-item">
+            <h3>Views</h3>
+            <p class="views-js">${image.views}</p>
+          </li>
+          <li class="list-item">
+            <h3>Comments</h3>
+            <p class="comments-js">${image.comments}</p>
+          </li>
+          <li class="list-item">
+            <h3>Downloads</h3>
+            <p class="downloads-js">${image.downloads}</p>
+          </li>
+        </ul>
+    </li>`;
+  }
+    export function galleryTemplate(array) {
+    return array.map(photo => photoTemplate(photo)).join('');
 }
 
-export function renderImages(images) {
+export function showElem(elem) {
+    elem.classList.remove('visually-hidden');
+}
 
-    const markup = images.hits.map((image) => {
-       return `<li class="images-list-item">
-  <a class="img-link" href=${image.largeImageURL} onclick="event.preventDefault()"><img class="img" src=${image.webformatURL} alt=${image.tags}></img></a>
-   <ul class="img-dscr">
-      <li class="img-data">
-        <p class="img-data-title">Likes</p>
-        <p class="img-data-numbers">${image.likes}</p>
-      </li>
-      <li class="img-data">
-        <p class="img-data-title">Views</p>
-        <p class="img-data-numbers">${image.views}</p>
-      </li>
-      <li class="img-data">
-        <p class="img-data-title">Comments</p>
-        <p class="img-data-numbers">${image.comments}</p>
-      </li>
-      <li class="img-data">
-        <p class="img-data-title">Downloads</p>
-        <p class="img-data-numbers">${image.downloads}</p>
-      </li>
-    </ul>
-</li>`;
-    }).join('\n');
-
-    refs.imageList.innerHTML = markup;
+export function hideElem(elem) {
+    elem.classList.add('visually-hidden');
+   
 }
